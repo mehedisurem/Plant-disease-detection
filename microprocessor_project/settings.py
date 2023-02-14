76d65d7dd63436cfd175cde217e529ca4b1baeb2
mehedisurem <mehedi.surem5@gmail.com>
2023-02-15 01:46:29 +0600

    3rd  commit

--- microprocessor_project/settings.py
+++ microprocessor_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ksk__+e=rryr0u_q68m1nnm54vun2vwkivxll@&u!+^^+xs^bd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
