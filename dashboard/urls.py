c5afc537bbda2f35ec61ad1872b087145f45caff
mehedisurem <mehedi.surem5@gmail.com>
2023-02-27 06:56:28 +0600

    Draft-1

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,8 +4,18 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('about/', views.about, name="AboutUs"),
-    path('pdf/',views.pdf, name="pdf"),
-    path ('sadia/',views.sadia, name="sadia"),
-    path ('mehenaz/',views.mehenaz, name="mehenaz"),
-    path ('surem/',views.surem, name="surem"),
+    path('pdf/', views.pdf, name="pdf"),
+    path('sadia/', views.sadia, name="sadia"),
+    path('mehenaz/', views.mehenaz, name="mehenaz"),
+    path('surem/', views.surem, name="surem"),
+    path('istiaq/', views.istiaq, name="istiaq"),
+    path('rafi/', views.rafi, name="rafi"),
+    path('jahedul/', views.jahedul, name="jahedul"),
+    path('system/', views.system, name="system"),
+    path('hardwere/', views.hardwere, name="hardwere"),
+    path('merge/', views.merge, name="merge"),
+    path('draft/', views.draft, name="draft"),
+    path('survey_form/', views.survey_form, name="survey_form"),
+    path('survey_ques/', views.survey_ques, name="survey_ques"),
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,22 +1,58 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return render(request, 'dashboard/overview.html')
-from django.shortcuts import render
-from django.http import  HttpResponse
+    return render(request, 'dashboard/test.html')
+
 
 def about(request):
     return render(request, 'dashboard/about.html')
+
+
 def pdf(request):
     return render(request, 'dashboard/test.html')
+
 
 def sadia(request):
     return render(request, 'dashboard/sadia.html')
 
+
 def mehenaz(request):
     return render(request, 'dashboard/mehenaz.html')
 
+
 def surem(request):
     return render(request, 'dashboard/surem.html')
+
+
+def istiaq(request):
+    return render(request, 'dashboard/istiaq.html')
+
+
+def rafi(request):
+    return render(request, 'dashboard/rafi.html')
+
+
+def jahedul(request):
+    return render(request, 'dashboard/jahedul.html')
+
+def system(request):
+    return render(request, 'dashboard/system-Diagram.html')
+
+
+def hardwere(request):
+    return render(request, 'dashboard/hardware-Design.html')
+
+def merge(request):
+    return render(request, 'dashboard/merged-summary.html')
+
+def draft(request):
+    return render(request, 'dashboard/draft.html')
+
+def survey_form(request):
+    return render(request, 'dashboard/survey-form.html')
+
+def survey_ques(request):
+    return render(request, 'dashboard/survey-questions.html')
